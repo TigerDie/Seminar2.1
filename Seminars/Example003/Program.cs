02916ac7e0f7855c36c727282c2e9178dce4b13c
Maxim Kisarev <collpse@mail.ru>
2022-09-30 13:55:36 +0500

    First Seminar Completed

---
+++ Seminars/Example003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input three-digit number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last Digit of {n} is {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Uncorrect input!");
+}
--- Seminars/Seminar1/Example002/Program.cs
+++ Seminars/Seminar1/Example002/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current;
+
+if(number > 0)
+{
+    current = -number;
+}
+else
+{
+    current = number;
+    number *= -1;
+}
+while(current <= number)
+{
+    Console.Write(current + " " );
+    current++;
+}
+/* 253 % 10 -> 3
+   253 % 100 -> 53
+   253 % 1000 -> 253
+
+   253 / 10 -> 25
+   */
