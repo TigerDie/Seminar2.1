b274bc5c1caa53dcfb7d0a3eaac260b984eec8ef
Maxim Kisarev <collpse@mail.ru>
2022-09-30 13:20:10 +0500

    add Seminar 1 info

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+// Задача 1. текст задачи
+
+Console.Write("Input a first number: ");
+int n1= Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int n2= Convert.ToInt32 (Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if(n1 == quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+Console.Write("Hello");
